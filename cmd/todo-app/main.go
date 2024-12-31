c31dacc6aba507c74f304fd1853a65ee536ee8bf
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-12-31 14:01:35 +0530

    MongoDB Connection

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"todo-list-app/pkg/mongo"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	client := mongo.ConnectDB()
+	defer client.Disconnect()
+
+	r := mux.NewRouter()
+
+	fmt.Println("Server Started At PORT 8000")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
